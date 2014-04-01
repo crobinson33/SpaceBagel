3f2e5fed0905a8716360a115e3ddfb5c511a4e36
josdento <josephndenton@gmail.com>
2014-03-31 23:21:03 -0500

    refactor

--- Graphics/Color.cs
+++ Graphics/Color.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using SFML.Graphics;
 
 namespace SpaceBagel
 {
 	public class Color
 	{
+		internal SFML.Graphics.Color color;
+		public float R;
+		public float G;
+		public float B;
+		public float A;
+
 		public Color ()
 		{
+			color.R = 1.0f;
+			color.G = 1.0f;
+			color.B = 1.0f;
+			color.A = 1.0f;
 		}
 	}
 }
